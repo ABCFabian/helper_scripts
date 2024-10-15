d5350f88b428d998e6bdb0b0a475195ca0bfc8fd
Fabian Beck <fabian.beck@aliceandbob.company>
2024-10-15 10:29:53 +0200

    add assume-role bash script

---
+++ aws/get-console-access/assume-role.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the required command-line argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: source assume_role.sh <role-arn>"
+    return 1
+fi
+
+ROLE_ARN=$1
+SESSION_NAME="AWSCLI-Session"
+
+# Assume the role using AWS CLI and capture the output
+ASSUME_ROLE_OUTPUT=$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "$SESSION_NAME")
+
+# Check if the assume-role command was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to assume role $ROLE_ARN"
+    return 1
+fi
+
+# Extract credentials using jq
+AWS_ACCESS_KEY_ID=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.AccessKeyId')
+AWS_SECRET_ACCESS_KEY=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SecretAccessKey')
+AWS_SESSION_TOKEN=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SessionToken')
+
+# Export the credentials to the current session
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+
+
+echo "Successfully assumed role $ROLE_ARN and exported credentials to session."
+# Open new process
+zsh
